b410569cc709e0d5875a0c53df2b42552dfe00bb
GGI <goshomaster97@gmail.com>
2022-10-21 11:12:53 +0300

    Updated and Formated Settings.py

--- Game_store_project/settings.py
+++ Game_store_project/settings.py
@@ -32,7 +32,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'Game_store_project.middlewares.NoPermissionTemplate'
 ]
 
 ROOT_URLCONF = 'Game_store_project.urls'
